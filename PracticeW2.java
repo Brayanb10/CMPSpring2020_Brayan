9651087bf66b06a9add5a167f3e58d12a777764e
Brayanb10 <60441010+Brayanb10@users.noreply.github.com>
2020-02-03 12:43:03 -0500

    Add files via upload

---
+++ PracticeW2.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @author Student Brayan
+ *created: 2/3/2020
+ *description: Variable Practice
+ **/
+public class PracticeW2 {
+	
+	public static void main(String[]args) {
+		//Defining Variables
+		double wallet = 10.35;
+		byte num = 5; 
+		int digit = 3; 
+		
+		//Declaring Variables 
+	    String name1; 
+		String name2;
+		String name3;
+		
+		//Assigning Values
+
+		name1= "mary";
+		name2="John";
+		name3="Theressa";
+		
+		//Printing
+		System.out.println(name1);
+		System.out.println(name1+name3);//Expected: MaryThesa
+		
+	}
+	
+	
+
+
+
+}
