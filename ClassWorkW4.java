c4829f56e234c68a90eb28a938e8653b52b067c8
Brayanb10 <60441010+Brayanb10@users.noreply.github.com>
2020-02-26 12:40:56 -0500

    Add files via upload

---
+++ ClassWorkW4.java
@@ -0,0 +1,17 @@
+/**
+ * @author Student: Brayan Batista
+ * creator=02-26-2020
+ * description: Write a program thats prints all number from 5 to 99.
+ */
+
+import java.util.Scanner;
+public class ClassWorkW4 {
+	public static void main(String[] args) {
+		int Start = 5;
+		while (Start <= 99) {
+			System.out.println(Start);
+			Start++;
+			
+		}
+}
+}
